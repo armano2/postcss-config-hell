07a61fe24b76dc2bea403b739612d01a5f830263
Armano <armano2@users.noreply.github.com>
2021-11-26 13:46:15 +0100

    minimal repro

--- .postcssrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-normalize'),
-    require('postcss-color-mod-function'),
-    require('postcss-preset-env')
-  ]
-};
--- webpack/aliases.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const aliases = {
-  root: path.resolve(__dirname, '../'),
-  src: path.resolve(__dirname, '../src/'),
-  styles: path.resolve(__dirname, '../src/styles')
-};
-
-module.exports = aliases;
